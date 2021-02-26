e73674ef89e19a506324cf1e4e43b519f05b1821
SwanX1 <karliscern@gmail.com>
2021-02-26 23:49:19 +0200

    Added configuration file and CLI arguments Port config, CLI flag --port Hostname config, CLI flag --host HTTPS config, CLI flag --https SSL Certificate Directory, CLI flag --cert

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
+const http = require('http');
 const https = require('https');
 const app = require('./app');
+const json5 = require('json5');
+
+// Set config variables
+const config = json5.parse(fs.readFileSync('../config.json5'));
+let port, hostname;
+// '??' operator not used, because the port cannot be 0
+port = Number(getArgument('--port', '-P')) || config.port || Number(process.env.PORT) || 8080;
+hostname = getArgument('--host', '-H') ?? config.host ?? process.env.HOST ?? '0.0.0.0';
 
 if (!process.cwd().endsWith('src'))
     process.chdir('src');
 
-const server = https.createServer({
-    key: fs.readFileSync('../cert/private.key'),
-    cert: fs.readFileSync('../cert/certificate.crt')
-}, app);
+let server;
+if (config.https || hasFlag('--https')) {
+    const certPath = path.join('..', getArgument('--cert', '-C') || config.certdir || 'cert');
+    server = https.createServer({
+        key: fs.readFileSync(path.join(certPath, 'private.key')),
+        cert: fs.readFileSync(path.join(certPath, 'certificate.crt'))
+    }, app);
+} else {
+    server = http.createServer(app);
+}
 
-if (!process.env.PORT) // Get environment PORT, command line PORT, default to 443.
-    process.env.PORT = process.argv.slice(2).find((value, index, array) => array[index - 1] === '--port') || 443;
+server.listen(port, hostname, () => console.log('Listening on port', port));
 
-server.listen(Number(process.env.PORT), '0.0.0.0', () => console.log('Listening on port', Number(process.env.PORT)));
+/**
+ * Gets arguments from process.argv,
+ * returns undefined if not set
+ * @param  {...string} identifiers
+ */
+function getArgument(...identifiers) {
+    let value;
+    for (const identifier of identifiers) {
+        value = process.argv.slice(2).find((value, index, array) => array[index - 1] === identifier);
+        if (value) break;
+    }
+    return value ?? null; // Return null if undefined
+}
+
+/**
+ * Returns true if any flags have been set in process.argv
+ * @param  {...string} identifiers
+ */
+function hasFlag(...identifiers) {
+    return process.argv.slice(2).some(value => identifiers.includes(value));
+}
